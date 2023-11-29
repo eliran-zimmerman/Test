38bb0f44c9c3d2e10d0880c26904505762da0d6a
Zimmerman <eliran.zimmerman@intel.com>
2023-11-29 15:21:15 +0200

    impl

---
+++ tests/tests.py
@@ -0,0 +1,23 @@
+import unittest
+import sys
+
+
+class Test(unittest.TestCase):
+    def __init__(self, testName, path) -> None:
+        super(Test, self).__init__(testName) 
+        self.path = path
+
+    def test_calc(self):
+        test_dict = {}
+        with open(self.path, "r") as f:
+            data = f.readlines()
+        for line in data:
+            line_s = line.split(":")
+            test_dict[line_s[0]] = line_s[1]
+        self.assertEqual(float(test_dict["output"]), (float(test_dict["input"]) - 32) / 1.8, "Should be same")
+
+
+path = sys.argv[1]
+suite = unittest.TestSuite()
+suite.addTest(Test('test_calc', path))
+unittest.TextTestRunner(verbosity=2).run(suite)
